1adae5a68827938a36faabb9611481a7b025eb8a
Kacper Nowicki <kacper.nowicki@wp.pl>
2014-03-13 18:45:24 +0100

    First checkin of bash command

---
+++ GetYahooQuote.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+echo "`date +'%F %T'` `wget -O - -l 1 http://finance.yahoo.com/q?s=USDPLN%3DX | awk '/yfs_l10_usdpln=x/ {print $0}' | sed 's/[<>]/\n/g' | grep -A 1 yfs_l10_usdpln=x | tail -1`" >> usdpln-yahoo.txt
